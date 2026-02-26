c1a1538c39cea144a56e153093198af6430de1e8
CongDN <111883652+StevenNgo25@users.noreply.github.com>
2026-02-26 23:43:15 +0700

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,26 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'admin/**',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: ':lang',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: () => Promise.resolve([{ lang: 'en' }, { lang: 'vi' }]),
+  },
+  {
+    path: ':lang/guide',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: () => Promise.resolve([{ lang: 'en' }, { lang: 'vi' }]),
+  },
+  {
+    path: ':lang/pricing',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: () => Promise.resolve([{ lang: 'en' }, { lang: 'vi' }]),
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
